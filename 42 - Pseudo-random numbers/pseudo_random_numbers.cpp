e149ba335d0ae4063268de06920653be8cb561fe
danielfdez17 <daniel17dev@gmail.com>
2023-11-19 18:27:46 +0100

    WRONG-ANSWER

--- 42 - Pseudo-random numbers/pseudo_random_numbers.cpp
+++ 42 - Pseudo-random numbers/pseudo_random_numbers.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cstring>
 #include <unordered_set>
 using namespace std;
-
+#define MAX 10000
 int cases = 1;
 
 // Resuelve un caso de prueba, leyendo de la entrada la
@@ -14,30 +15,34 @@ bool resuelveCaso() {
     int z, i, m, l; cin >> z >> i >> m >> l;
     if (z == 0 && i == 0 && m == 0 && l == 0) return false;
     int first_random = l;
-    // int sequence = 0;
-    // int next = (z * l + i) % m;
-    // l = next;
     int next, sequence = 0;
-    unordered_set<int>visitados;
-    visitados.clear();
+    // unordered_set<int>visitados;
+    // visitados.clear();
+    bool visited[MAX];
+    memset(visited, 0, MAX * sizeof(bool));
+    visited[l] = true;
     // while (next != first_random && visitados.count(next) == 0) {
     //     visitados.insert(next);
-    //     next = (z * l + i) % m;
+    //     // next = (z * l + i) % m;
     //     l = next;
     //     ++sequence;
     // }
     // if (next == first_random) ++sequence;
     while (true) {
-        next = (z * l + i) % m;
+        // next = (z * l + i) % m;
+        long long prod = ((z % m) * (l % m)) % m;
+        next = ((prod % m) + (i % m)) % m;
         if (next == first_random) {
             sequence++;
             break;
         }
-        else if (visitados.count(next) != 0) {
-            break;
-        }
+        else if (visited[next]) break;
+        // else if (visitados.count(next) != 0) {
+        //     break;
+        // }
         l = next;
-        visitados.insert(next);
+        visited[next] = true;
+        // visitados.insert(next);
         sequence++;
     }
     cout << "Case " << cases << ": " << sequence << "\n";
