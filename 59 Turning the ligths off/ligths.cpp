a4c6b92ca775dd963634b0c3b4c606fcce0ab585
danielfdez17 <daniel17dev@gmail.com>
2023-12-14 15:14:04 +0100

    first approach

--- 59 Turning the ligths off/ligths.cpp
+++ 59 Turning the ligths off/ligths.cpp
@@ -6,8 +6,43 @@
 using namespace std;
 
 #define SIZE 10
+#define OPTIONS 4
 
 char board[SIZE][SIZE];
+
+int movs[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+bool ok(int i, int j) {
+    return 0 <= i && i < SIZE && 0 <= j && j < SIZE;
+}
+
+bool isOn(int x, int y) {
+    return board[x][y] == 'O';
+}
+
+void changeState(int x, int y) {
+    if (isOn(x, y)) board[x][y] = '#';
+    else board[x][y] = 'O';
+}
+
+void turn(int x, int y) {
+    changeState(x, y);
+    for (int i = 0; i < OPTIONS; ++i) {
+        int newx = x + movs[i][0], newy = y + movs[i][1];
+        if (ok(newx, newy)) {
+            changeState(x, y);
+        }
+    }
+}
+
+void show() {
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            cout << board[i][j];
+        }
+        cout << "\n";
+    }
+}
 
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
@@ -20,6 +55,30 @@ bool resuelveCaso() {
             char c; cin >> c;
             board[i][j] = c;
         }
+    }
+    show();
+
+    if (word == "all_off") {
+        cout << word << " 0\n";
+    }
+    else {
+        int ligths = 0;
+        // for (int i = 0; i < SIZE; ++i) {
+        //     if (isOn(0, i)) {
+        //         turn(0, i);
+        //         ++ligths;
+        //     }
+        // }
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                if (isOn(i, j)) {
+                    turn(i + 1, j);
+                    ++ligths;
+                }
+            }
+        }
+        if (ligths > 100) ligths = -1;
+        cout << word << " " << ligths << "\n";
     }
     
 
